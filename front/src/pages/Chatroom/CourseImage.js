408993e2c8ebd3fe0666d15564e7f92e77daf4e3
yang.bo <yang.bo@eland.co.kr>
2019-05-05 22:21:27 +0800

    #12 课件图片缩放，关闭，用户查询刷新

---
+++ front/src/pages/Chatroom/CourseImage.js
@@ -0,0 +1,43 @@
+import React, { PureComponent } from "react";
+import { connect } from "dva";
+import { Col, Button, Modal, Checkbox, Card, List, Popover, message, Icon } from "antd";
+import CourseWareSelect from "./../CourseWareManage/CourseWareSelect"
+
+
+class CourseImage extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible: false,
+        }
+    }
+    hide = () => {
+        this.setState({
+            visible: false,
+        });
+    }
+    handleVisibleChange = (visible) => {
+        this.setState({ visible });
+    }
+
+    render() {
+        var src = this.props.src || ""
+        const content = (
+            <div>
+                <img src={src + "?x-oss-process=image/resize,m_lfit,h_500,w_500"} alt="" />
+                < Icon type="close" onClick={this.hide} style={{ position: "absolute", top: "15px", right: "20px",backgroundColor:"white" }} />
+            </div>
+        )
+
+        return (
+            <Popover placement="rightTop" title=''
+                content={content} trigger="click"
+                visible={this.state.visible}
+                onVisibleChange={this.handleVisibleChange}
+            >
+                <img src={src} alt="" />
+            </Popover>
+        )
+    }
+}
+export default CourseImage;
